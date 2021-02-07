9db4aecca16cadf866b46ee612fb431316c0e2f8
thiagoreis96 <thiago.reis96@icloud.com>
2021-02-07 10:11:06 -0300

    Enviando últimos exercícios de estrutura sequencial

---
+++ logica_java_udemy/src/estrutura_sequencial/Uri1018.java
@@ -0,0 +1,58 @@
+/* 
+ * Para não poluir esta página, não coloquei o enunciado do exercício aqui.
+ * Caso deseje ver o enunciado do mesmo, por favor utilize o link abaixo.
+ * Link: https://www.urionlinejudge.com.br/judge/pt/problems/view/1018
+ */
+
+package estrutura_sequencial;
+
+import java.util.Scanner;
+
+public class Uri1018 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N, quociente, resto, nota;
+		
+		N = sc.nextInt();
+		System.out.println(N);
+
+		resto = N;
+
+		nota = 100;
+		quociente = resto / nota;
+		System.out.println(quociente + " nota(s) de R$ " + nota + ",00");
+	    resto = resto % nota;
+
+		nota = 50;
+		quociente = resto / nota;
+		System.out.println(quociente + " nota(s) de R$ " + nota + ",00");
+	    resto = resto % nota;
+
+		nota = 20;
+		quociente = resto / nota;
+		System.out.println(quociente + " nota(s) de R$ " + nota + ",00");
+	    resto = resto % nota;
+
+		nota = 10;
+		quociente = resto / nota;
+		System.out.println(quociente + " nota(s) de R$ " + nota + ",00");
+	    resto = resto % nota;
+
+		nota = 5;
+		quociente = resto / nota;
+		System.out.println(quociente + " nota(s) de R$ " + nota + ",00");
+	    resto = resto % nota;
+
+		nota = 2;
+		quociente = resto / nota;
+		System.out.println(quociente + " nota(s) de R$ " + nota + ",00");
+	    resto = resto % nota;
+
+		System.out.println(resto + " nota(s) de R$ 1,00");
+
+		sc.close();
+	}
+}
